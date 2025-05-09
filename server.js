68207a55f78c07295a8de9441fa7903a1f0ec36b
Utkarshni <theutkarshni@gmail.com>
2025-05-10 01:46:38 +0530

    Final server.js ready for deployment

--- server.js
+++ server.js
@@ -67,6 +67,8 @@ app.post("/contact", async (req, res) => {
   }
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`🚀 Server is running on http://localhost:${process.env.PORT}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on port ${PORT}`);
 });
+
